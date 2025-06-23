2979ec1f2c93493619c3d62805c0eb5ac9f235b9
Thanaphat Ch. <coconutistaster@gmail.com>
2025-06-23 15:55:01 +0700

    Add files via upload

---
+++ cell03/ex03/advance_mult.py
@@ -0,0 +1,12 @@
+def advanced_mult():
+   i = 0
+   while i <= 10:
+      print("Table no.", i, end=": ")
+      j = 0
+      while j <= 10:
+         print(i*j, end="  ")
+         j += 1
+      print(" ")
+      i += 1
+
+advanced_mult()
