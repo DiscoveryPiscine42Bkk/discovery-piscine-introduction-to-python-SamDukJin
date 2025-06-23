b0343035ad0106c215d453c7e4638381d72d74f9
Thanaphat Ch. <coconutistaster@gmail.com>
2025-06-23 12:52:26 +0700

    Add files via upload

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,4 @@
+first_name = input("What is your first name?: ").title().strip()
+last_name = input("Great. What is your last name?: ").title().strip()
+
+print("Pleased you meet you, " + first_name + " " + last_name) 
