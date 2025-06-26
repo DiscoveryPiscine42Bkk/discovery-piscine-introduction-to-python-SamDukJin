05dd0b8774072fb2bf5a15887f088f41b3d62be5
Thanaphat Ch. <coconutistaster@gmail.com>
2025-06-26 15:04:01 +0700

    Fixed and create the ex01

---
+++ Rush/ex01/main.py
@@ -0,0 +1,21 @@
+import sys
+from checkmate import checkmate
+
+def main():
+    if len(sys.argv) == 1:
+        print("no file inserted.")
+    else:
+        for filename in sys.argv[1:]:
+            try:
+                print("Opening file:", filename)
+                with open(filename, 'r') as file:
+                    print("Open succeed and reading", filename)
+                    content = file.read().strip()
+                    checkmate(content)
+                    print("Done!")
+            except Exception as e:
+                print("Error:", e)
+
+if __name__ == "__main__":
+    main()
+
