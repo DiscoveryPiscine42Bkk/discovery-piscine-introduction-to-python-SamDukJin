825ceddfd7ce137f73a19037c66e36da925d2983
Thanaphat Ch. <coconutistaster@gmail.com>
2025-06-26 15:12:29 +0700

    Update main.py

--- Rush/ex00/main.py
+++ Rush/ex00/main.py
@@ -11,19 +11,6 @@ def main():
 .....\
 """
       checkmate(board)
-   # Next is for the ex01, where we accept the file using the python file reading function.
-   else:
-      
-      for filename in sys.argv[1:]:
-         try:
-               print("Opening file:", filename)
-               with open(filename, 'r') as file:
-                  print("Open succeed and reading", filename)
-                  content = file.read().strip()
-                  checkmate(content)
-                  print("Done!")
-         except Exception as e:
-               print("Error:", e)
 
 if __name__ == "__main__":
     main()
